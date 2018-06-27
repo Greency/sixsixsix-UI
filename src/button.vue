2921004e6a20e9b8ba879dc3b046199bca315f3c
Greency <1015561170@qq.com>
2018-06-27 19:17:02 +0800

    添加 loading button

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
-  <button class="s-button" :class="[(iconPosition === 'right') ? 'icon-right' : '']">
-    <s-icon class="icon" v-if="icon" :name="icon"></s-icon>
+  <button class="s-button" :class="[(iconPosition === 'right') ? 'icon-right' : '']" @click="$emit('click')">
+    <s-icon class="icon" v-if="icon && !loading" :name="icon"></s-icon>
+    <s-icon class="icon loading" v-if="loading" name="loading"></s-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -10,57 +11,75 @@
 export default {
   props: {
     icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
     iconPosition: {
       type: String,
       default: 'left',
-      validator (value){
-        return value === 'left' || value === 'right' 
+      validator(value) {
+        return value === 'left' || value === 'right'
       }
     }
+  },
+  methods: {
+
   }
 }
 </script>
 <style lang="scss">
-  .s-button{ 
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    font-size: var(--font-size);
-    height: var(--btn-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background:var(--btn-bg);
-    vertical-align: top;
-    
-    &:hover{
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.s-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: var(--font-size);
+  height: var(--btn-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--btn-bg);
+  vertical-align: top;
+
+  &:hover {
     border-color: var(--border-color-hover);
-    }
-    &:active{
-      background-color: var(--btn-active-bg);
-    }
-    &:focus{
-      outline: none;
-    }
+  }
+  &:active {
+    background-color: var(--btn-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
 
-    > .icon{
-      order: 1;
-      margin-right: .1em;
-    }
-    > .content{
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .icon {
       order: 2;
+      margin-left: 0.1em;
+      margin-right: 0;
     }
-
-    &.icon-right{
-      >.icon{
-        order: 2;
-        margin-left: .1em;
-        margin-right: 0;
-      }
-      >.content{
-       order: 1;
-      }
+    > .content {
+      order: 1;
     }
   }
-  
+  .loading {
+    animation: spin 1.5s infinite linear;
+  }
+}
 </style>
