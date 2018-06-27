409fa7467acfc73c3c66645402af233b4d8c1f4e
Greency <1015561170@qq.com>
2018-06-27 22:43:13 +0800

    添加 button group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './buttonGroup.vue'
 
 Vue.component('s-button', Button)
 Vue.component('s-icon', Icon)
+Vue.component('s-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/buttonGroup.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="s-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  mounted(){
+    for(let node of this.$el.children){
+      if(node.nodeName !== 'BUTTON'){
+        console.warn('s-button-group的子元素必须是s-button')
+      }
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .s-button-group{
+    display: inline-flex;
+    vertical-align: top;
+
+    > .s-button{
+      border-radius: 0;
+      &:not(:first-child){
+        margin-left: -1px;
+      }
+      &:first-child{
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child{
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover{
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
