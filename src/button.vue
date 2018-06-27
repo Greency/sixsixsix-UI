27364445224dbc3f14cc3d9cbeb4980e9eb30a53
Greency <1015561170@qq.com>
2018-06-27 10:17:18 +0800

    利用parcel实现button单文件

---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="s-button">按钮</button>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  .s-button{
+    font-size: var(--font-size);
+    height: var(--btn-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background:var(--btn-bg);
+    
+    &:hover{
+    border-color: var(--border-color-hover);
+    }
+    &:active{
+      background-color: var(--btn-active-bg);
+    }
+    &:focus{
+      outline: none;
+    }
+  }
+  
+</style>
